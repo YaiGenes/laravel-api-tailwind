47c7b85b2d07bcd7cf4c3345f7b568a5321f1d0d
Yaiser <yaiserar@gmail.com>
2022-01-22 18:18:07 +0100

    feat starting apiV2 && GET output with model interrelation post/author

--- app/Http/Controllers/Api/V2/PostController.php
+++ app/Http/Controllers/Api/V2/PostController.php
@@ -1,11 +1,14 @@
 <?php
 
-namespace App\Http\Controllers\Api\V1;
+namespace App\Http\Controllers\Api\V2;
 
 use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\Request;
-use App\Http\Resources\V1\PostResource;
+
+use App\Http\Resources\V2\PostResource;
+use App\Http\Resources\V2\PostCollection;
+
 
 class PostController extends Controller
 {
@@ -16,8 +19,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        return PostResource::collection(Post::latest()->paginate());
         //
+        return new PostCollection();
     }
 
     /**
@@ -28,7 +31,7 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //return
+        //
     }
 
     /**
@@ -38,7 +41,7 @@ class PostController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(Post $post)
-    {   
+    {
         return new PostResource($post);
         //
     }
@@ -63,8 +66,6 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        Post::where('id', $post->id)->delete();
-        return response()->json(['message'=>'Post deleted successfully'], 204);
         //
     }
 }
